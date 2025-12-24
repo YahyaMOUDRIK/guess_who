494ac5f4d2e2e346b3bd821c27794efc8535f45b
Yahya Moudrik <yahya.moudrik.00@gmail.com>
2025-12-25 00:07:13 +0100

    Guess Who multiplayer game2

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // For Socket.IO to work properly
-  webpack: (config) => {
-    config.externals.push({
-      bufferutil: "bufferutil",
-      "utf-8-validate": "utf-8-validate",
-    });
-    return config;
-  },
+  turbopack: {},
 };
 
 module.exports = nextConfig;
-
